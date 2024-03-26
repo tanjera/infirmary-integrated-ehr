5bd275241c6694e4384174d8e60cd375e125bd48
Ibi Keller <ibi.keller@gmail.com>
2024-03-26 15:57:03 -0400

    0.0.4: Login Layout and Links @ Welcome - Added @auth on welcome.blade   - If already logged in, card displays to proceed to dashboard - Added "Register" link onto login-snippet.blade   - Layout adjusted

--- base/resources/views/auth/login-snippet.blade.php
+++ base/resources/views/auth/login-snippet.blade.php
@@ -33,15 +33,28 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+        <div class="{{--flex items-center justify-end mt-4--}} relative grid grid-cols-2 w-max gap-6 p-2 items-center">
+            <div class="grid grid-rows-2 gap-2 flex justify-start">
+                <div>
+                @if (Route::has('register'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                        {{ __('Register') }}
+                    </a>
+                @endif
+                </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+                <div>
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+                </div>
+            </div>
+            <div class="row-span-2 flex justify-end">
+                <x-primary-button class="ms-3">
+                    {{ __('Log in') }}
+                </x-primary-button>
+            </div>
         </div>
     </form>
--- base/resources/views/welcome.blade.php
+++ base/resources/views/welcome.blade.php
@@ -37,11 +37,24 @@
 
                                 </div>
 
-                                <div class="relative flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-                                    <div class="relative max-w-sm justify-center gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
-                                        @include('auth/login-snippet')
-                                    </div>
-                                </div>
+                                @if (Route::has('login'))
+                                    @auth {{-- Already authenticated? --}}
+                                        <div class="relative flex flex-col items-center selection:bg-[#FF2D20] selection:text-white">
+                                            <div class="relative max-w-sm px-3 items-center justify-center text-center gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                                            <a href="{{ url('/dashboard') }}" class="rounded-md py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white" style="font-weight: 700">
+                                                Already Logged In<br>
+                                                Proceed to the Dashboard
+                                            </a>
+                                        </div>
+                                        </div>
+                                    @else
+                                        <div class="relative flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+                                            <div class="relative max-w-sm justify-center gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                                                @include('auth/login-snippet')
+                                            </div>
+                                        </div>
+                                    @endauth
+                                @endif
 
                                 <div class="relative flex items-center gap-6 lg:items-end">
                                     <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
